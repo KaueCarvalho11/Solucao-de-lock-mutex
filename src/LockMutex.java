313ffc06deeccec73812c1f5a43ccb9085d236b8
Jean-Richardson <jeanrichardson88@gmail.com>
2025-06-28 10:35:04 -0300

    update final

--- src/LockMutex.java
+++ src/LockMutex.java
@@ -18,10 +18,9 @@ public class LockMutex {
     private static int sharedCounter = 0;
     private static final SimpleMutex mutex = new SimpleMutex();
     private static final int ITERATIONS = 1_000_000;
-}
 
     public static void main(String[] args) throws InterruptedException {
-        // Define a primeira tarefa: incrementa sharedCounter ITERATIONS vezes
+       
         Runnable counterTask1 = () -> {
             try {
                 for (int i = 0; i < ITERATIONS; i++) {
@@ -38,7 +37,7 @@ public class LockMutex {
             }
         };
 
-        // Define a segunda tarefa: faz o mesmo trabalho
+       
         Runnable counterTask2 = () -> {
             try {
                 for (int i = 0; i < ITERATIONS; i++) {
@@ -55,19 +54,19 @@ public class LockMutex {
             }
         };
 
-        // Cria as threads
+      
         Thread t1 = new Thread(counterTask1, "Thread-Contador-1");
         Thread t2 = new Thread(counterTask2, "Thread-Contador-2");
 
-        // Inicia as duas threads
+        
         t1.start();
         t2.start();
 
-        // Aguarda ambas terminarem
         t1.join();
         t2.join();
 
-        // Exibe o valor final; deve ser 2 * ITERATIONS
+        
         System.out.printf("Valor final de sharedCounter = %d (esperado %d)%n",
                           sharedCounter, 2 * ITERATIONS);
-    }
+    }
+}
